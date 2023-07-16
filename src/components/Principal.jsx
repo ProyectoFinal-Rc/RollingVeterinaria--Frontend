85f281f233bfaf0a7e9d813b096cc7e8d66f1ebe
Mauricio Soria <mauricioutn2017@gmail.com>
2023-07-16 16:09:08 -0300

    Video con letras y estilizado de colores seleccionado, frase agregada sobre el video y posicionada correctamente

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -5,23 +5,38 @@ const Principal = () => {
     return (
         <>
             <section id="video" className="position-relative overflow-hidden">
-                <video className="position-absolute z-n1 d-none d-md-block" src={Video} autoPlay muted loop />
                 <section className="pb-5 container-fluid">
                     <div id="tituloFraseGrande">
                         <div className="display-2 d-flex flex-column flex-md-row justify-content-center align-items-center gap-md-3 py-3" id="animacionPrincipal">
                             <div className="contenedorTextoAnimacion">
-                                <span id="TextoGrupo1" className="text-primary slide-texto-fromBottom">Transformá</span>
+                                <span id="TextoGrupo1" className="slide-texto-fromBottom rollingVet-negro">Estamos Aqui</span>
                             </div>
                             <div className="contenedorTextoAnimacion">
-                                <span id="TextoGrupo2" className="text-secondary slide-texto-fromTop">tu cuerpo</span>
+                                <span id="TextoGrupo2" className=" slide-texto-fromTop  rollingVet-naranja">para Ellos</span>
                             </div>
                         </div>
-                        <h3 className="h1 text-center">
-                            <strong className="letrero texto-naranja">¡El éxito </strong>
-                            <strong className="letrero texto-azul">comienza aquí!</strong>
-                        </h3>
                     </div>
                 </section>
+                <video className="position-absolute z-n1 d-none d-md-block" src={Video} autoPlay muted loop />
+                <img className="position-absolute top-50 start-50 translate-middle z-n1 d-md-none h-100" src="./img/cierrese-arriba-muchas-pesas-gimnasia-metal-estante-gimnasio-deporte.jpg" alt="" />
+                <Container id="texto" className="py-3">
+                    <Row className="d-flex align-items-center justify-content-center">
+                        <Col xs={12} md={6} className=''>
+                            <div>
+                                <h1 className="display-5 fw-bold text-light mb-3">
+                            {/*<strong className="rollingfitLogo-primary">El cuidado y amor que tu mascota se merece <br />
+                                </strong> */}
+                                    <strong className="rollingVet-naranja"><strong className='rollingVet-negro'> Suscribite HOY </strong> <br />Y comenzá a disfrutar de los beneficios!</strong>
+                                </h1>
+                            </div>
+                        </Col>
+                        <Col xs={12} md={6} className='text-center'>
+                            <Button className="btn btn-primary rollingVet-naranja rounded-pill btn-lg px-5 " id='boton' href="#nuestrosTestimonios" role="button">
+                                ¡Suscribite!
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
             </section>
         </>
     );
