63b086eea28b3cf55a222e6f364874e0f27a85b5
Mauricio Soria <mauricioutn2017@gmail.com>
2023-07-17 21:12:17 -0300

    solucionado problema responsive en seccion de servicios

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -26,14 +26,12 @@ const Principal = () => {
                     </div>
                 </section>
                 <video className="position-absolute z-n1 d-none d-md-block" src={Video} autoPlay muted loop />
-                <img className="position-absolute top-50 start-50 translate-middle z-n1 d-md-none h-100" src="./img/cierrese-arriba-muchas-pesas-gimnasia-metal-estante-gimnasio-deporte.jpg" alt="" />
+                <img className="position-absolute top-50 start-50 translate-middle z-n1 d-md-none h-100" src="https://images.pexels.com/photos/5731861/pexels-photo-5731861.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
                 <Container id="texto" className="py-3">
                     <Row className="d-flex align-items-center justify-content-center">
                         <Col xs={12} md={6} className=''>
-                            <div>
+                            <div id='tituloFrase'>
                                 <h1 className="display-5 fw-bold text-light mb-3">
-                                    {/*<strong className="rollingfitLogo-primary">El cuidado y amor que tu mascota se merece <br />
-                                </strong> */}
                                     <strong className="rollingVet-naranja"><strong className='rollingVet-negro'> Suscribite HOY </strong> <br />Y comenzá a disfrutar de los beneficios!</strong>
                                 </h1>
                             </div>
@@ -49,11 +47,11 @@ const Principal = () => {
             <section id='planes' className='container my-2 border-rounded'>
                 <Card>
                     <Row className='flex-row justify-content-center my-3'>
-                        <Col xs={12} md={4} className=''>
+                        <Col xs={12} sm={12} md={4} lg={4} className=''>
                             <h1 className='text-start ms-3 rollingVet-negro'>Planes de cuidado</h1>
                             <p className='text-start ms-3 rollingVet-naranja'>Nuestra veterinaria ofrece un servicio exclusivo de planes de cuidado mensuales diseñados para garantizar la salud y el bienestar óptimo de las mascotas a lo largo de diferentes etapas de su vida. Estos planes están diseñados pensando en las necesidades específicas de cada etapa, brindando un enfoque proactivo y completo para el cuidado de las mascotas.</p>
                         </Col>
-                        <Col xs={12} md={6} className='d-flex justify-content-center'>
+                        <Col xs={12} sm={12} md={6} lg={6} className='d-flex justify-content-center'>
                             <Card className='mx-1' rounded>
                                 <Card.Body>
                                     <Card.Title className='text-center rollingVet-negro'>Primeros Pasos</Card.Title>
@@ -86,55 +84,58 @@ const Principal = () => {
                 </Card>
             </section>
             <section id='servicios' className='container my-2 border-rounded'>
-                <Container id='serviciosContenedor'>
-                    <div className='d-flex justify-content-around px-5 py-4'>
-                        <Col className='col-md-6 d-flex justify-content-center  flex-column mx-3'>
-                            <img className='servicio-imagen zoom-imagen' variant="top" src={Servicio1} />
-                            <div>
-                                <h3>Cirugias Generales</h3>
-                                <p>
-                                    Atencion las 24hs para mantener siempre a tu mejor amigo en buen estado, con la seguridad y aval de RollingVet.
-                                </p>
-                                <a className='texto-marron'>Ver mas</a>
-                            </div>
-                        </Col>
-                        <Col className='col-md-6 d-flex justify-content-center  flex-column'>
-                            <img className='servicio-imagen zoom-imagen' variant="top" src={Servicio2} />
-                            <div>
-                                <h3>Peluqueria</h3>
-                                <p>
-                                    Estilizado y baños, a cargo de profesionales capacitados para darle el look que ellos se merecen.
-                                </p>
-                                <a className='texto-marron'>Ver mas</a>
-                            </div>
-                        </Col>
-                    </div>
-                </Container>
-                <Container id='serviciosContenedor'>
-                    <div className='d-flex justify-content-around px-5 py-4'>
-                        <Col className='col-md-6 d-flex justify-content-center  flex-column mx-3'>
-                            <img className='servicio-imagen zoom-imagen' variant="top" src={Servicio3} />
-                            <div>
-                                <h3>Pet Shop</h3>
-                                <p>
-                                    Tenemos la mas amplia variedad de juguetes,ropa y accesorios!. Vení y mirá como ellos tambien se pueden entrenetener con nuestros productos.
-                                </p>
-                                <a className='texto-marron'>Ver mas</a>
-                            </div>
-                        </Col>
-                        <Col className='col-md-6 d-flex justify-content-center  flex-column'>
-                            <img className='servicio-imagen zoom-imagen' variant="top" src={Servicio4} />
-                            <div>
-                                <h3>Vacunacion y Control</h3>
-                                <p>
-                                    Realizadas en nuestra sala de Vacunacion, llevamos a cabo el control rutinario de todos los refuerzos necesarios para mantenerlos Sanos y Fuertes
-                                </p>
-                                <a className='texto-marron'>Ver mas</a>
-                            </div>
-                        </Col>
-                    </div>
-                </Container>
-
+                <Row id='serviciosContenedor' className='d-flex justify-content-center flex-row mx-3'>
+                    <Col className='d-flex justify-content-center flex-column my-3 mx-3'>
+                        <div className='container d-flex'>
+                            <img className='servicio-imagen mt-3 zoom-imagen' variant="top" src={Servicio1} />
+                        </div>
+                        <div className="container textoServicios">
+                            <h3>Cirugias Generales</h3>
+                            <p>
+                                Atención las 24hs para mantener siempre a tu mejor amigo en buen estado, con la seguridad y aval de RollingVet.
+                            </p>
+                            <a className='texto-marron'>Ver más</a>
+                        </div>
+                    </Col>
+                    <Col className='d-flex justify-content-center flex-column my-3 mx-3'>
+                        <div className='container d-flex'>
+                            <img className='servicio-imagen mt-3 zoom-imagen' variant="top" src={Servicio2} />
+                        </div>
+                        <div className="container textoServicios">
+                            <h3>Peluqueria</h3>
+                            <p>
+                                Estilizado y baños, a cargo de profesionales capacitados para darle el look que ellos se merecen.
+                            </p>
+                            <a className='texto-marron'>Ver más</a>
+                        </div>
+                    </Col>
+                </Row>
+                <Row id='serviciosContenedor' className='d-flex justify-content-center flex-row mx-3'>
+                    <Col className='d-flex justify-content-center flex-column my-3 mx-3'>
+                        <div className='container d-flex '>
+                            <img className='servicio-imagen mt-3 zoom-imagen' variant="top" src={Servicio3} />
+                        </div>
+                        <div className="container textoServicios">
+                            <h3>Pet Shop</h3>
+                            <p>
+                                Tenemos la más amplia variedad de juguetes, ropa y accesorios. ¡Vení y mirá cómo ellos también pueden entretenerse con nuestros productos!
+                            </p>
+                            <a className='texto-marron'>Ver más</a>
+                        </div>
+                    </Col>
+                    <Col className='d-flex justify-content-center flex-column my-3 mx-3'>
+                        <div className='container d-flex '>
+                            <img className='servicio-imagen mt-3 zoom-imagen' variant="top" src={Servicio4} />
+                        </div>
+                        <div className="container textoServicios">
+                            <h3>Vacunación y Control</h3>
+                            <p>
+                                Realizadas en nuestra sala de Vacunación, llevamos a cabo el control rutinario de todos los refuerzos necesarios para mantenerlos Sanos y Fuertes.
+                            </p>
+                            <a className='texto-marron'>Ver más</a>
+                        </div>
+                    </Col>
+                </Row>
             </section>
             <section id='colaboradores'>
                 <Container className='bg-white'>
@@ -209,7 +210,7 @@ const Principal = () => {
                 </Container>
             </section>
             <section id='testimonios'>
-            
+
             </section>
         </>
     );
