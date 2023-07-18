b731a0fe5a3259f311810e3707c321c7665f896c
Malena de Arriba <dearribamalena@gmail.com>
2023-07-17 23:43:18 -0300

    agregue el componente de EditarTurno, con su modal y formulario validado

--- src/components/views/admins/ItemTurno.jsx
+++ src/components/views/admins/ItemTurno.jsx
@@ -1,7 +1,15 @@
 import { Button } from "react-bootstrap";
+import EditarTurno from "./EditarTurno";
+import { useState } from "react";
+
+
 
 const ItemTurno = ({cita}) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
+    <>
     <tr>
       <td>{cita.detalleCita}</td>
       <td>{cita.veterinario}</td>
@@ -9,10 +17,12 @@ const ItemTurno = ({cita}) => {
       <td>{cita.fecha}</td>
       <td>{cita.hora}</td>
       <td className="d-flex justify-content-end align-items-star">
-        <Button className="btn btn-warning me-2"><i className="bi bi-pencil-square p-0"></i></Button>
+        <Button className="btn btn-warning me-2"onClick={handleShow}><i className="bi bi-pencil-square p-0"></i></Button>
         <Button variant="danger"><i className="bi bi-file-x p-0"></i></Button>
       </td>
     </tr>
+    <EditarTurno show={show} handleClose={handleClose}></EditarTurno>
+    </>
   );
 };
 
