a4c65da175b0712103b76e12c8d9b409b3d91e86
juanmllanos <juan_m@hotmail.com.ar>
2023-07-18 23:04:18 -0300

    se agrego color de fondo y logo

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Nav, Container, Navbar } from 'react-bootstrap'
 import './Navbar.css'
+import logo from '/logo.png'
 
 const Navegacion = () => {
 	return (
-		<Navbar bg='info' expand='lg' id='navbar'>
+		<Navbar expand='lg' id='navbar'>
 			<Container>
-				<Navbar.Brand href='#'>Logo VET</Navbar.Brand>
+				<Navbar.Brand href='#'><img src={logo} className='imagenLogo'></img></Navbar.Brand>
 				<Navbar.Toggle area-aria-controls='basic-navbar-var'></Navbar.Toggle>
 				<Navbar.Collapse id='basic-navbar-var'>
 					<Nav className='ms-auto'>
