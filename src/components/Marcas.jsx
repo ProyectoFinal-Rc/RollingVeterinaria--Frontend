9d5cc7d508097b2813ac58f9c20b74e6e74acfab
Mauricio Soria <mauricioutn2017@gmail.com>
2023-07-18 23:49:25 -0300

    Agregado slider infinito de marcas

--- src/components/Marcas.jsx
+++ src/components/Marcas.jsx
@@ -1,9 +1,104 @@
 import React from 'react';
 import Imagen1 from "../assets/Marcas/eukanuba.png"
+import Imagen2 from "../assets/Marcas/Miproyecto.png"
+import Imagen3 from "../assets/Marcas/whiskas.png"
+import Imagen4 from "../assets/Marcas/proplan.png"
+import Imagen5 from "../assets/Marcas/vitalcan.png"
+import Imagen6 from "../assets/Marcas/sieger.png"
+import Imagen7 from "../assets/Marcas/agility.png"
+import Imagen8 from "../assets/Marcas/nupec.png"
+import Imagen9 from "../assets/Marcas/monello.png"
 const Marcas = () => {
     return (
-        <div className='d-flex justify-content-center bg-white'>
-        <img src={Imagen1} style={{width:"250px"}} alt="" />
+        <div className='d-flex justify-content-center align-items-center  bg-white slider'>
+            <div className="slide-track">
+                <div className="slide mx-4 mt-1">
+                    <img src={Imagen1} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-2">
+                    <img src={Imagen2} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-3">
+                    <img src={Imagen3} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-4">
+                    <img src={Imagen4} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-5">
+                    <img src={Imagen5} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-0">
+                    <img src={Imagen6} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mb-1">
+                    <img src={Imagen7} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mb-2">
+                    <img src={Imagen8} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mb-3">
+                    <img src={Imagen9} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+
+
+
+                <div className="slide mx-4 mb-4">
+                    <img src={Imagen1} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mb-5">
+                    <img src={Imagen2} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-5">
+                    <img src={Imagen3} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-4">
+                    <img src={Imagen4} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-3">
+                    <img src={Imagen5} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-2">
+                    <img src={Imagen6} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-1">
+                    <img src={Imagen7} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-0">
+                    <img src={Imagen8} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-4">
+                    <img src={Imagen9} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+
+                <div className="slide mx-4 mt-1">
+                    <img src={Imagen1} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-2">
+                    <img src={Imagen2} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-3">
+                    <img src={Imagen3} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-4">
+                    <img src={Imagen4} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-5">
+                    <img src={Imagen5} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mt-0">
+                    <img src={Imagen6} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mb-1">
+                    <img src={Imagen7} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mb-2">
+                    <img src={Imagen8} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+                <div className="slide mx-4 mb-3">
+                    <img src={Imagen9} className="marcas" style={{ width: "250px" }} alt="" />
+                </div>
+
+            </div>
         </div>
     );
 };
