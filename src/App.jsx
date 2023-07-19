bf730a89462f098a1ac4379040c7fdf99ad1ed1f
juanmllanos <juan_m@hotmail.com.ar>
2023-07-19 18:34:23 -0300

    validaciones html terminadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import Navegacion from './components/common/Navbar'
-import Login from './components/Login'
 
 function App() {
 
   return (
     <>
-      <Login></Login>
       <Navegacion></Navegacion>
     </>
   )
