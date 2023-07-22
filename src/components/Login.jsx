d87bc32dea5074dab0611dce72d835a53330be05
Malena de Arriba <67029623+mmaleducada@users.noreply.github.com>
2023-07-22 15:00:20 -0300

    Delete Login.jsx
    Se elimina para evitar conflictos

--- src/components/Login.jsx
+++
@@ -1,6 +0,0 @@
-import './Login.css'
-import { Form, Container } from "react-bootstrap"
-import { useForm } from 'react-hook-form'
-import { IniciarSesion } from "./helpers/queriesLogin"
-import Swal from "sweetalert2"
-
