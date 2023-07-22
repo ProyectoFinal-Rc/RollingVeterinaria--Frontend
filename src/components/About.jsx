039aff16866d6e77f66f83bd6118d4a2a8879f38
smlich <facudecano09@gmail.com>
2023-07-22 01:13:29 -0300

    dejo estructura lista, fantan cargar datos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,48 +4,51 @@ import './about.css'
 
 const developers = [
   {
-    nombre: "Desarrollador 1",
-    lenguajes: ["JavaScript", "Python", "HTML", "CSS"],
+    nombre: "",
+    foto: "/src/assets/patita.png",
+    lenguajes: ["HTML", "CSS", "JavaScript"],
     descripcion:
-      "Un apasionado desarrollador de software con experiencia en desarrollo web y programación.",
+      "Un apasionado desarrollador web con experiencia en front-end y diseño de sitios atractivos y funcionales.",
+    animalFavorito: "",
   },
   {
-    nombre: "Desarrollador 2",
-    lenguajes: ["Java", "C++", "SQL"],
+    nombre: "Mauricio Soria",
+    foto: "/src/assets/patita.png",
+    lenguajes: ["JavaScript", "React", "Node.js"],
     descripcion:
-      "Un experto en lenguajes de programación orientados a objetos y desarrollo de aplicaciones.",
+      "Apasionado por la tecnología , siempre en busqueda de nuevos conocimientos.",
+    animalFavorito: "Perro",
   },
-  {
-    nombre: "Desarrollador 3",
-    lenguajes: ["Java", "C++", "SQL"],
-    descripcion:
-      "Un experto en lenguajes de programación orientados a objetos y desarrollo de aplicaciones.",
-  },
-  // Añade aquí más objetos con información sobre otros desarrolladores
+
 ];
 
 const About = () => {
 
   return (
     <>
+        <div className="banner">
+            <h1>Nuestro Equipo</h1>
+        </div>
       <Container>
-      <h1 className="my-5">Equipo de Desarrolladores</h1>
-      <Row>
-        {developers.map((developer, index) => (
-          <Col md={4} key={index}>
-            <Card className="mb-4">
-              <Card.Body>
-                <Card.Title>{developer.nombre}</Card.Title>
-                <Card.Text>
-                  <strong>Lenguajes:</strong> {developer.lenguajes.join(", ")}
-                </Card.Text>
-                <Card.Text>{developer.descripcion}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+        <Row>
+          {developers.map((developer, index) => (
+            <Col md={4} key={index}>
+              <Card className="mb-4">
+                <Card.Body className="cardBackgound">
+                  <Card.Title className="fuentePatitas asco">{developer.nombre}</Card.Title>
+                  <Card.Img variant="top" src={developer.foto} /><Card.Img />
+                  <Card.Text className="asco">{developer.descripcion}</Card.Text>
+                  <Card.Text>
+                    <strong>Lenguajes:</strong> {developer.lenguajes.join(", ")}
+                  </Card.Text>
+                  <Card.Text><strong>Animal Favorito:</strong> {developer.animalFavorito}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      
     </>
   )
 }
