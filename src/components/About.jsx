57d4c462049dd545b2d7c35d0bd8abbd21f8e969
smlich <facudecano09@gmail.com>
2023-07-22 01:54:34 -0300

    corrijo estilos y agrego botones redes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 import './about.css'
 
 const developers = [
@@ -26,29 +27,40 @@ const About = () => {
 
   return (
     <>
-        <div className="banner">
-            <h1>Nuestro Equipo</h1>
-        </div>
+      <div className="banner">
+        <h1>Nuestro Equipo</h1>
+      </div>
       <Container>
         <Row>
           {developers.map((developer, index) => (
             <Col md={4} key={index}>
               <Card className="mb-4">
                 <Card.Body className="cardBackgound">
-                  <Card.Title className="fuentePatitas asco">{developer.nombre}</Card.Title>
+                  <Card.Title className="fuentePatitas">{developer.nombre}</Card.Title>
                   <Card.Img variant="top" src={developer.foto} /><Card.Img />
-                  <Card.Text className="asco">{developer.descripcion}</Card.Text>
+                  <Card.Text className="a">{developer.descripcion}</Card.Text>
                   <Card.Text>
                     <strong>Lenguajes:</strong> {developer.lenguajes.join(", ")}
                   </Card.Text>
                   <Card.Text><strong>Animal Favorito:</strong> {developer.animalFavorito}</Card.Text>
+                  <div className="social-buttons botonesRedes">
+                    <Button variant="" href="">
+                      <FaInstagram size={24} />
+                    </Button>
+                    <Button variant="" href="">
+                      <FaLinkedin size={24} />
+                    </Button>
+                    <Button variant="" href="">
+                      <FaGithub size={24} />
+                    </Button>
+                  </div>
                 </Card.Body>
               </Card>
             </Col>
           ))}
         </Row>
       </Container>
-      
+
     </>
   )
 }
