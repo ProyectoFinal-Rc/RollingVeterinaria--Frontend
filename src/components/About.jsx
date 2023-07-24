d2f05774052a57f0db3927634d7dc1370d1d5840
Malena de Arriba <dearribamalena@gmail.com>
2023-07-24 16:38:12 -0300

    se modifica la pagina de about para que quede responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,12 @@ import './about.css'
 
 const developers = [
   {
-    nombre: "",
+    nombre: "Juan Pablo Alba",
     foto: "/src/assets/patita.png",
     lenguajes: ["HTML", "CSS", "JavaScript"],
     descripcion:
-      "Un apasionado desarrollador web con experiencia en front-end y diseño de sitios atractivos y funcionales.",
-    animalFavorito: "",
+      "Un dedicado estudiante de programación. Tiene sólidas habilidades técnicas, así como excelentes habilidades interpersonales que le permiten liderar equipos",
+    animalFavorito: "Perro",
   },
   {
     nombre: "Mauricio Soria",
@@ -23,7 +23,7 @@ const developers = [
     foto: "/src/assets/kittens-cat-cat-puppy-rush-45170.png",
     lenguajes: ["JavaScript", "React", "Node.js","Html","Css"],
     descripcion:
-      "Male, una apasionada estudiante de programación fullstack. Con 26 años, su pasión por la tecnología y el desarrollo web la impulsan a aprender y crecer constantemente en este campo en constante evolución. Además de su dedicación a la programación, encuentra equilibrio y satisfacción en sus otros intereses: el deporte y la pintura",
+      "Male, una apasionada estudiante de programación fullstack. Además de su dedicación a la programación, encuentra equilibrio y satisfacción en sus otros intereses: el deporte y la pintura",
     animalFavorito: "Gato",
   },
   {
@@ -31,30 +31,44 @@ const developers = [
     foto: "/src/assets/patita.png",
     lenguajes: ["HTML", "CSS", "JavaScript"],
     descripcion:
-      "Un apasionado desarrollador web con experiencia en front-end y diseño de sitios atractivos y funcionales.",
+      "Un estudiante de desarrollo web con experiencia en front-end y diseño de sitios atractivos y funcionales.",
     animalFavorito: "Gato",
   },
+  {
+    nombre: "Juan Manuel Llanos",
+    foto: "/src/assets/patita.png",
+    lenguajes: ["HTML", "CSS", "JavaScript"],
+    descripcion:
+      "Un apasionado del mundo de la programacion, disfruta de la resolución de problemas y el trabajo en equipo",
+    animalFavorito: "Gato",
+  },
+  {
+    nombre: "Facundo Gomez",
+    foto: "/src/assets/patita.png",
+    lenguajes: ["HTML", "CSS", "JavaScript"],
+    descripcion:
+      "Un dedicado desarrollador web novato. Aunque es relativamente nuevo en el campo de la programación, su entusiasmo y sed de conocimiento son palpables.",
+    animalFavorito: "Gato",
+  }
 
 ];
 
 const About = () => {
 
   return (
-    <>
-      <div className="banner">
-        <h1 id="titulo-about">Nuestro Equipo</h1>
+      <Container className="mt-2">
+      <div className="text-center">
+      <h1 id="titulo-about" className="fw-bold text-center">EL EQUIPO</h1>
+      <img src="./src/assets/nosotros.png" alt="" className="imagen-nosotros" />
       </div>
-      <Container>
-        <Row>
+        <Row className="mt-4 text-center justify-content-center">
           {developers.map((developer, index) => (
-            <Col md={4} key={index}>
-              <Card className="mb-4">
+            <Col md={6} xs={10} lg={6} key={index}>
+              <Card className="mb-4" id="card-contenedor-about">
                 <Card.Body className="cardBackgound">
-                  <Card.Title className="fuentePatitas">{developer.nombre}</Card.Title>
-                  <Card.Img variant="top" src={developer.foto} className="card-img-top-about" /><Card.Img />
-                  <Card.Text className="compressed-text text-truncate overflow-hidden"><strong>
+                  <Card.Title className="fw-bold fs-2" id="card-nombre">{developer.nombre}</Card.Title>
+                  <Card.Text className="compressed-text">
                     {developer.descripcion}
-                    </strong>
                     </Card.Text>
                   <Card.Text>
                     <strong>Lenguajes y Tecnologias:</strong> {developer.lenguajes.join(", ")}
@@ -77,8 +91,6 @@ const About = () => {
           ))}
         </Row>
       </Container>
-
-    </>
   )
 }
 
