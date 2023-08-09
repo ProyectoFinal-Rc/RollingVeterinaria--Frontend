9a69b76039ff57fff688fb5a50801d6e21c4ea07
Malena de Arriba <dearribamalena@gmail.com>
2023-08-09 20:22:27 -0300

    modifique el titulo de acerca de nosotros

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <Container className="mt-3 fade-up">
       <div className="text-center mt-5">
-        <h1 className="fw-bold text-center titular">El equipo</h1>
+        <h1 className="fw-bold text-center titular">El Equipo</h1>
         <img src={Nosotros} alt="Imagenes de animales (dibujos animados)" className="imagen-nosotros" />
       </div>
       <Row className="mt-4 text-center justify-content-center">
